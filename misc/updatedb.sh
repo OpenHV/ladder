7d5263bc5d08006434b7bd3700e577d4b974d32b
Matthias Mailänder <matthias@mailaender.name>
2025-07-09 18:58:16 +0200

    Move web server documentation to the wiki.

--- misc/updatedb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -xeu
-
-~/venv/bin/openhv-ladder --bans-file /home/openhv/bans.list -d db-hv-all.sqlite3      /home/openhv/srv-ladder/instance-*/support_dir/Replays/
-~/venv/bin/openhv-ladder --bans-file /home/openhv/bans.list -d db-hv-2m.sqlite3 -p 2m /home/openhv/srv-ladder/instance-*/support_dir/Replays/
-
-cp -v db-*-*.sqlite3  /home/web/venv/var/web-instance/
