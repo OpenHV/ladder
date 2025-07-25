69e65c573adb834bb0e5fb9c2031251cbbc63d9f
Matthias Mailänder <Mailaender@users.noreply.github.com>
2025-07-25 18:23:17 +0200

    Update to version 20250725.

--- web/mods.py
+++ web/mods.py
@@ -2,7 +2,7 @@ mods = dict(
     hv=dict(
         label="Hard Vacuum",
         icon="icon-hv.png",
-        url="https://github.com/OpenHV/OpenHV/releases/tag/20250715",
-        release="20250715",
+        url="https://github.com/OpenHV/OpenHV/releases/tag/20250725",
+        release="20250725",
     )
 )
